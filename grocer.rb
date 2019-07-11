2ede98d5b3351e56671785320e5cb0ad7d8009d3
Anthony Joseph Marchitelli Jr <amarchitelli1993@gmail.com>
2019-07-11 22:21:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,19 +1,4 @@
-def consolidate_cart(array)
-  final = Hash.new 0 
-  count = :count
-item.each do |hash|
-  hash.each do |food, description|
-  
-if final.has_key?(food) == false
-  final[food] = description
-  final[food][count] = 1
-elsif final.has_key?(food)
-final[food][:count] +=1
-end
-end
-end
-final
-end.
+
 
 def apply_coupons(cart, coupons)
   # code here
